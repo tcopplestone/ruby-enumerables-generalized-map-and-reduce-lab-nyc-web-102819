94df2d160cd3e910ad73a60a47823ee9cd812322
Trewin Copplestone <tcopplestone@gmail.com>
2019-10-25 14:08:49 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,16 @@
-# Your Code Here
+def map (source_array)
+  new_array = []
+  counter = 0
+  while counter < source_array.length do
+    new_array.push(yield(source_array[counter]))
+    counter += 1
+  end
+  return new_array
+end
+
+def reduce (source_array, starting_point = 0)
+  return yield(starting_point, source_array)
+
+  
+
+  
